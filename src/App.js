e4a3a28891a555c94f18f5d3e8a8a4b2db24b298
nitinuttarwar007 <nitin.uttarwar007@gmail.com>
2019-03-04 00:47:14 +0530

    Rendering a Sidebar with React Router v4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom'
 import './App.css';
+
+const routes = [
+  { path: '/',
+    exact: true,
+    sidebar: () => <div>home!</div>,
+    main: () => <h2>Home</h2>
+  },
+  { path: '/categoris',
+    sidebar: () => <div>categoris!</div>,
+    main: () => <h2>Categoris</h2>
+  },
+  { path: '/locations',
+    sidebar: () => <div>locations!</div>,
+    main: () => <h2>Locations</h2>
+  }
+]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Router>
+          <div style={{ display: 'flex' }}>
+            <div style={{
+              padding: '10px',
+              width: '40%',
+              background: '#f0f0f0'
+            }}>
+              <ul style={{ listStyleType: 'none', padding: 0 }}>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/categoris">Categoris</Link></li>
+                <li><Link to="/locations">Locations</Link></li>
+              </ul>
+              {routes.map((route) => (
+                <Route
+                  key={route.path}
+                  path={route.path}
+                  exact={route.exact}
+                  component={route.sidebar}
+                />
+              ))}
+            </div>
+            <div style={{ flex: 1, padding: '10px' }}>
+              {routes.map((route) => (
+                <Route
+                  key={route.path}
+                  path={route.path}
+                  exact={route.exact}
+                  component={route.main}
+                />
+              ))}
+            </div>
+          </div>
+        </Router>
       </div>
     );
   }
